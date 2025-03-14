97de71a7677a0baaacb65d39be673bf51a59d735
Vaggelis Magonezos <nospipian@gmail.com>
2025-03-14 11:19:26 +0200

    Update clerk-client.provider.ts

--- apps/api/src/providers/clerk-client.provider.ts
+++ apps/api/src/providers/clerk-client.provider.ts
@@ -1,6 +1,6 @@
 import { createClerkClient } from '@clerk/backend';
 import { ConfigService } from '@nestjs/config';
-
+//
 export const ClerkClientProvider = {
   provide: 'ClerkClient',
   useFactory: (configService: ConfigService) => {
