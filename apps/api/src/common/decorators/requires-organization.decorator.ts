e7401c7e279fdb6fd3d5400b7e6ac588bf96ab35
Vaggelis Magonezos <nospipian@gmail.com>
2025-08-15 18:22:19 +0300

    Delete requires-organization.decorator.ts

--- apps/api/src/common/decorators/requires-organization.decorator.ts
+++
@@ -1,6 +0,0 @@
-// src/decorators/requires-organization.decorator.ts
-import { SetMetadata } from '@nestjs/common';
-
-export const REQUIRES_ORGANIZATION_KEY = 'requiresOrganization';
-export const RequiresOrganization = () =>
-  SetMetadata(REQUIRES_ORGANIZATION_KEY, true);
