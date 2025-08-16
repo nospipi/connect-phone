3751e18cb9f61e354bcfd4eeaf2560e4f9a9aa6f
Vaggelis Magonezos <nospipian@gmail.com>
2025-08-16 12:04:51 +0300

    Update core.module.ts

--- apps/api/src/common/core/core.module.ts
+++ apps/api/src/common/core/core.module.ts
@@ -3,13 +3,28 @@ import { Module, Global } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RequestContextService } from './request-context.service';
 import { CurrentOrganizationService } from './current-organization.service';
+import { CurrentDbUserService } from './current-db-user.service';
+import { CurrentClerkUserService } from './current-clerk-user.service';
+import { OrganizationContextService } from './organization-context.service';
 import { User } from '../../database/entities/user.entity';
 import { Organization } from '../../database/entities/organization.entity';
 
 @Global()
 @Module({
   imports: [TypeOrmModule.forFeature([User, Organization])],
-  providers: [RequestContextService, CurrentOrganizationService],
-  exports: [RequestContextService, CurrentOrganizationService],
+  providers: [
+    RequestContextService,
+    CurrentClerkUserService,
+    CurrentDbUserService,
+    CurrentOrganizationService,
+    OrganizationContextService,
+  ],
+  exports: [
+    RequestContextService,
+    CurrentClerkUserService,
+    CurrentDbUserService,
+    CurrentOrganizationService,
+    OrganizationContextService,
+  ],
 })
 export class CoreModule {}
