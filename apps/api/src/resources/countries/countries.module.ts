8663a018c808121f1b32868f9910157dac46bb3c
Vaggelis Magonezos <62842853+nospipi@users.noreply.github.com>
2025-10-08 17:29:21 +0300

    updates

--- apps/api/src/resources/countries/countries.module.ts
+++ apps/api/src/resources/countries/countries.module.ts
@@ -4,12 +4,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CountryEntity } from '@/database/entities/country.entity';
 import { GetAllCountriesOfOrgController } from './services/get-all-countries-of-org/controller';
 import { GetAllCountriesOfOrgService } from './services/get-all-countries-of-org/service';
+import { GetCountryByIdController } from './services/get-country-by-id/controller';
+import { GetCountryByIdService } from './services/get-country-by-id/service';
 import { UpdateCountryController } from './services/update-country/controller';
 import { UpdateCountryService } from './services/update-country/service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CountryEntity])],
-  controllers: [GetAllCountriesOfOrgController, UpdateCountryController],
-  providers: [GetAllCountriesOfOrgService, UpdateCountryService],
+  controllers: [
+    GetAllCountriesOfOrgController,
+    GetCountryByIdController,
+    UpdateCountryController,
+  ],
+  providers: [
+    GetAllCountriesOfOrgService,
+    GetCountryByIdService,
+    UpdateCountryService,
+  ],
 })
 export class CountriesModule {}
