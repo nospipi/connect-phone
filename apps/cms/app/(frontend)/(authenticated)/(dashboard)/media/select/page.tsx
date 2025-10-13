cc6ce8a6ce368abcb7e46d4a8306d69d531faeb8
Vaggelis Magonezos <62842853+nospipi@users.noreply.github.com>
2025-10-13 19:12:56 +0300

    Update page.tsx

--- apps/cms/app/(frontend)/(authenticated)/(dashboard)/media/select/page.tsx
+++ apps/cms/app/(frontend)/(authenticated)/(dashboard)/media/select/page.tsx
@@ -73,7 +73,7 @@ const Page = async ({ searchParams }: PageProps) => {
       {/* Header */}
       <div className="flex items-center gap-4 border-b border-gray-200 bg-white dark:border-gray-800 dark:bg-gray-950">
         <Link
-          href="/sales-channels"
+          href={previousPage}
           className="flex h-full items-center justify-center px-4 py-2 text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-900 dark:hover:text-gray-300"
         >
           <RiArrowLeftLine className="h-4 w-4" />
