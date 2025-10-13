55df6aadcfa8daed3570754c89a68af6a33da218
Vaggelis Magonezos <62842853+nospipi@users.noreply.github.com>
2025-10-13 15:17:29 +0300

    updates

--- apps/cms/app/(frontend)/(authenticated)/(dashboard)/media/page.tsx
+++ apps/cms/app/(frontend)/(authenticated)/(dashboard)/media/page.tsx
@@ -1,6 +1,15 @@
+import Link from "next/link"
+
+//-------------------------------------------------------------------
+
 const Page = async () => {
   await new Promise((resolve) => setTimeout(resolve, 1500)) //simulate loading
-  return <div>Media Page</div>
+  return (
+    <div>
+      Media Page
+      <Link href="/media/select">Go to Select Media Page</Link>
+    </div>
+  )
 }
 
 export default Page
---
+++ apps/cms/app/(frontend)/(authenticated)/(dashboard)/media/select/page.tsx
@@ -0,0 +1,6 @@
+const Page = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 1500)) //simulate loading
+  return <div>Select Media Page</div>
+}
+
+export default Page
